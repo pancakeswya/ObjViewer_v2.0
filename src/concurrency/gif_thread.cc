dc4cfe1a417fe72d51cccb1819a572d9642ee9bd
niyaz <winfordt@LAPTOP-3I75CPIJ>
2024-01-04 03:07:51 +0300

    gif assert added

--- src/concurrency/gif_thread.cc
+++ src/concurrency/gif_thread.cc
@@ -3,31 +3,29 @@
 #include "base/gif_settings.h"
 #include "third_party/gif.h"
 
+#define GIF_ASSERT(__status) \
+  if (!__status) {           \
+    emit Failed();           \
+    return;                  \
+  }
+
 namespace objv {
 
 void GifThread::run() {
   GifWriter gif_wr;
   bool is_savable = GifBegin(&gif_wr, path_.toStdString().c_str(), kGifWidth,
                              kGifHeight, kGifDelay, kGifBitWidth, kGifDither);
-  if (!is_savable) {
-    emit Failed();
-    return;
-  }
+  GIF_ASSERT(is_savable);
   for (unsigned int timer = 0; timer <= kGifDuration; timer += kGifFrameDelay) {
     emit Running();
     bool is_written = GifWriteFrame(
         &gif_wr, frame_.convertToFormat(QImage::Format_RGBA8888).constBits(),
         kGifWidth, kGifHeight, kGifDelay, kGifBitWidth, kGifDither);
-    if (!is_written) {
-      emit Failed();
-      return;
-    }
+    GIF_ASSERT(is_written);
     QThread::msleep(kGifFrameDelay);
   }
   bool is_succeed = GifEnd(&gif_wr);
-  if (!is_succeed) {
-    emit Failed();
-  }
+  GIF_ASSERT(is_succeed);
 }
 
 }  // namespace objv
